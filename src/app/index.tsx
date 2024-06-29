cb21f3737e80c58f0e6010c80e4e002975081f16
Kohagura Maimu <e225711@ie.u-ryukyu.ac.jp>
2024-06-29 15:28:16 +0900

    マークアップメモスクリーン

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,57 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Index = (): JSX.Element => {
     return (
-        <View>
-            <Text>Hello World</Text>
+        <View style={styles.container}>
+            <View>
+                <View>
+                    <Text>Memo App</Text>
+                    <Text>ログアウト</Text>
+                </View>
+            </View>
+            <View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2021/01/01</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2021/01/01</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+                <View>
+                    <View>
+                        <Text>買い物リスト</Text>
+                        <Text>2021/01/01</Text>
+                    </View>
+                    <View>
+                        <Text>X</Text>
+                    </View>
+                </View>
+            </View>
+            <View>
+                <Text>+</Text>
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, //画面いっぱいに広げる
+        justifyContent: 'center', //縦方向の中央寄せ
+        alignItems: 'center' //横方向の中央寄せ
+    }
+})
 
 export default Index;
  
